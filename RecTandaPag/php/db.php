9ea963ac19105bbef59d8a213c0fad22e1c75d48
nahuelflores <arcangel6191@gmail.com>
2021-05-31 12:08:15 -0300

    Correccion en tablas, agregado de auto_increment a las claves primarias de las tablas

---
+++ RecTandaPag/php/db.php
@@ -0,0 +1,19 @@
+<?php
+
+class dbModel{
+
+    private $db;
+
+    function __construct(){
+        $this->db = new PDO('mysql:host=localhost;'.'dbname=bd_recolector;charset=utf8', 'root', '');
+    }
+     
+    function GetUser($email){
+        $query = $this->db->prepare("SELECT * FROM users WHERE email=?");
+        $query->execute(array($email));
+        return $query->fetch(PDO::FETCH_OBJ);
+    }
+
+}
+
+?>
